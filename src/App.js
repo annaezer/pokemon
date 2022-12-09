5eb87cc8281108a8c02a16ad105c48f22e3e53fc
annaezer <annaezer@gmail.com>
2022-12-09 15:48:40 +0100

    Twintig kaartjes op pagina

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import './App.css';
 import axios from 'axios'
 import Pokemon from "./components/Pokemon";
 
-    function App() {
+function App() {
+    const [pokemons, setPokemons] = useState([]);
 
-  return (
-    <>
-        <header>
-            <h1>Pokemon</h1>
-            <button> Vorige </button>
-            <button> Volgende </button>
-        </header>
-        <main>
-            <Pokemon/>
-        </main>
-    </>
-  );
+    useEffect(() => {
+        async function getCards() {
+            try {
+                const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+                const twentyPokemon = response.data.results;
+                console.log(twentyPokemon);
+                setPokemons(twentyPokemon);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        getCards();
+    }, [])
+
+    return (
+        <>
+            {Object.keys(pokemons).length > 0 &&
+                <>
+                    <header>
+                        <h1>Pokemon</h1>
+                        <button> Vorige</button>
+                        <button> Volgende</button>
+                    </header>
+                    <main>
+                        <ul>
+                            {pokemons.map((pokemon) => {
+                                return <Pokemon name={pokemon.name} key={pokemons.name}/>
+                            })
+                            }
+                        </ul>
+                    </main>
+                </>}
+        </>
+    );
 }
 
 export default App;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,16 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
-function Pokemon(props) {
+function Pokemon({name}) {
     const [pokemon, setPokemon] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff');
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
                 setPokemon(response.data);
                 console.log(pokemon);
-            } catch(e) {
+            } catch (e) {
                 console.error(e);
             }
         }
@@ -19,18 +19,20 @@ function Pokemon(props) {
 
     return (
         <>
-            <article>
-                <h2>{pokemon.name}</h2>
-                <p><strong>Moves: </strong>{pokemon.moves.length} </p>
-                <p> <strong>Weight: </strong>{pokemon.weight}</p>
-                <p><strong>Abilities: </strong></p>
-                <ul>
-                    {pokemon.abilities.map((poke) => {
-                        return (
-                            <li key={pokemon.id}> {poke.ability.name}</li>
-                        )
-                    })} </ul>
-            </article>
+            {Object.keys(pokemon).length > 0 &&
+                <>
+                    <article>
+                        <h2>{pokemon.name}</h2>
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+                        <p><strong>Moves: </strong>{pokemon.moves.length} </p>
+                        <p><strong>Weight: </strong>{pokemon.weight}</p>
+                        <p><strong>Abilities: </strong></p>
+                        <ul>
+                            {pokemon.abilities.map((poke) => {
+                                return <li key={pokemon.name}> {poke.ability.name}</li>
+                            })} </ul>
+                    </article>
+                </>}
         </>
     );
 }
